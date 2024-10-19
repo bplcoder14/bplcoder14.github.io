7a2d9861bb77b2600a36a9ffc785b7bd542db3b3
Braiden Larson <blarson31@huskers.unl.edu>
2024-10-19 00:12:42 -0500

    setup base for the website;

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div class="fixed-bottom">
+      <figure class="text-center">
+        <blockquote class="blockquote">
+          <p>It always seems impossible until it's done</p>
+        </blockquote>
+        <figcaption class="blockquote-footer">
+          <cite title="Source Title">Nelson Mandela</cite>
+        </figcaption>
+      </figure>
+    </div>
+  );
+}
+
+export default Footer;
